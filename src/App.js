c4ec14b35233c30687b4cbef9de13cc59a933bd1
Ayush1234567434324 <104828221+Ayush1234567434324@users.noreply.github.com>
2023-10-06 00:01:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Frame, ImursCorporate } from './component/pricing/price';
+import {NavbarLanding} from '../navbarLanding/navbar';
 
 function App() {
   return (
     <div >
+    <NavbarLanding/>
 <ImursCorporate/>
     </div>
   );
